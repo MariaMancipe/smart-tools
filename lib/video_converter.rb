e06eb5e21df539c6d3a53a73182a66de16d9d4f3
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-09-24 15:27:55 -0500

    mal comentario

--- lib/video_converter.rb
+++ lib/video_converter.rb
@@ -18,7 +18,7 @@ def convert_to_mp4(path)
   new_path = $converted+ new_path[0,new_path.length-4]
 
   movie.transcode("#{new_path}.mp4", %w(-acodec aac -vcodec h264 -strict -2 -threads 1 -threads 1))
-  //move_upload_to_original(path)
+  #move_upload_to_original(path)
 end
 
 def search_files
