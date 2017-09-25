56f565653d96bd50a7147fad656cd632b92c6ad0
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-09-25 14:56:09 -0500

    video converter

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -43,8 +43,7 @@ class VideoUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
-    puts File.extname(original_filename)
-    original_filename if original_filename
+    model.nombre+"__"+model.id+File.extname(original_filename)
   end
 
 end
