e67ac016d36e46f1da92a0e65c99145570d45180
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-09-24 20:22:16 -0500

    marca el estado

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,11 @@
 class Video < ApplicationRecord
   belongs_to :concurso
   mount_uploader :video, VideoUploader
+  before_save :mark_state
+
+  def mark_state
+    puts self.video
+    self.estado ||= 1
+  end
   #Validations
 end
