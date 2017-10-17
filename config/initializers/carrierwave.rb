03e8e2acf31a433c3e0d24868954618226e193b7
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-10-17 16:54:54 -0500

    fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -16,7 +16,6 @@ CarrierWave.configure do |config|
   #   end
   # end
 
-  config.storage = :fog
   config.fog_provider = "fog/aws"
   config.fog_credentials = {
       :provider               => 'AWS',                             # required
@@ -28,4 +27,5 @@ CarrierWave.configure do |config|
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+  config.storage = :fog
 end
