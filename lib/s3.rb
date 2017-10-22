5d0a4f654edf6103a4724cf4457c7253f56c8179
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-10-22 17:57:10 -0500

    s3 example

--- lib/s3.rb
+++ lib/s3.rb
@@ -6,4 +6,4 @@ s3 = Aws::S3::Resource.new(
     secret_access_key: ENV['SECRET_ACCESS_KEY']
 )
 
-s3.bucket(ENV['S3_BUCKET']).object('/uploads/videos/video/video/12/Hola Colombia__9876543__.avi').get(response_target: '/public/examples')
+s3.bucket(ENV['S3_BUCKET']).object('Hola Colombia__9876543__.avi').get(response_target: '/public/examples')
