50d83ffe98a5958fcbf1c2d5f2b2891123567235
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-10-25 21:01:35 -0500

    prueba video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -25,10 +25,10 @@ class VideosController < ApplicationController
 
     @video = Videody.new(video_params)
     @video.save
-    #@concurso.videos.create(video_params)
-    #@video.concurso = @concurso
+    @concurso.videos.create(video_params)
+    @video.concurso = @concurso
 
-    json_response(@video, :created)
+    json_response(@concurso.videos, :created)
   end
 
   # PUT /concursos/:concurso_id/videos/:id
