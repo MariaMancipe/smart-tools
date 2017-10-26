ce62cb8df336313ea234db776ff29565f2f0f21b
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-10-26 15:02:28 -0500

    video

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,14 +4,12 @@ class Video < ApplicationRecord
   belongs_to :concurso
   mount_uploader :video, VideoUploader
   before_save :mark_state
-  after_save :send_to_queue
+
 
   def mark_state
     self.estado ||= 0
   end
 
-  def send_to_queue
-    UploaderQueuer::send_message_to_converter_queue('New video uploaded!', self.id, self.video_original, self.correo_concursante)
-  end
+
   #Validations
 end
