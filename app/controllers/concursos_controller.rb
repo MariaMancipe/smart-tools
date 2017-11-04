ee07fe84de31f150c7b74ec6bc903310ebe3b72f
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2017-11-04 18:11:02 -0500

    concursocrontroller

--- app/controllers/concursos_controller.rb
+++ app/controllers/concursos_controller.rb
@@ -39,7 +39,7 @@ class ConcursosController < ApplicationController
     puts concurso_params
     @concurso = Concursody.new(concurso_params)
     uploader = PictureUploader.new
-    uploader.store!(concurso_params.file)
+    uploader.store!(@concurso.picture.tempfile)
     @concurso.save
     @usuario.concursos.create(concurso_params)
 
